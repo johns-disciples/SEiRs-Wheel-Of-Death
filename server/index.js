3cff2bd0041de2efeb896ce91b7c090f6c78b8e3
Ash Coca <40476562+Ashcoca@users.noreply.github.com>
2019-05-16 15:56:51 -0700

    Adding students db file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const students = require('../DB/students.js');
 // const redis = require('redis');
 require('dotenv').config();
 
